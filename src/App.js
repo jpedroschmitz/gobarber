f146d79b8943679ea887afe07f3130c63af8195b
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-07-16 17:44:30 -0300

    chore: add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import history from './services/history';
 
-function App() {
-  return <div className="App" />;
+export default function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
-
-export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
