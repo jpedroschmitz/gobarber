60cb945c22c4c304546fa9db7252a217f10a0923
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-07-17 20:25:09 -0300

    chore: add root imports

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
-import { AuthLayout, DefaultLayout } from '../pages/_layouts';
+import { AuthLayout, DefaultLayout } from '~/pages/_layouts';
 
 export default function RouteWrapper({
   component: Component,
