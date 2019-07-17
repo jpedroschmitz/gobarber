5999e74d577d3121badbb207240338be816dbbda
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-07-17 20:11:02 -0300

    chore: add global style

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 export default function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
---
+++ src/styles/global.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: none;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
