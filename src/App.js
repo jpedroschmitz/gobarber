5cc4883e1240509d464c1cfb17992118fcaa1caf
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-07-18 09:43:38 -0300

    chore: add initial store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
@@ -6,13 +7,17 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
