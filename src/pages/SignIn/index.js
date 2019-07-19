fd5ba5e6b9b2b061320a65910c793daa0819a849
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-07-18 23:47:12 -0300

    chore: add loading to auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -16,6 +16,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -33,7 +34,7 @@ export default function SignIn() {
           placeholder="Sua senha secreta..."
         />
 
-        <button type="submit">Acessar</button>
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
 
         <Link to="/register">Criar conta gratuita</Link>
       </Form>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,25 +1,29 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 import api from '~/services/api';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, '/sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador.');
+    if (!user.provider) {
+      console.tron.error('Usuário não é prestador.');
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
